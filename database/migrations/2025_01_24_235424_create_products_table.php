44dc19f7fa15e5810ed3664172b67582072ebc11
Joao <jovmdcomenezes@gmail.com>
2025-01-31 14:43:29 -0300

    Products and Category enabled

--- database/migrations/2025_01_24_235424_create_products_table.php
+++ database/migrations/2025_01_24_235424_create_products_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->longText('description')->nullable();
             $table->decimal('price', 10, 2);
             $table->boolean('is_active')->default(true);
+            $table->boolean('is_featured')->default(false);
             $table->boolean('is_popular')->default(false);
             $table->boolean('in_stock')->default(true);
             $table->boolean('on_sale')->default(false);
